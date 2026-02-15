1eaff3b1b4a8fab6086abbeb535478545c3c5e32
Emre-M-Karatas <emre.karatas1@hotmail.com>
2026-02-15 16:54:11 +1100

    going on the java section now

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.querySelector(".input__btn");
 
-  // HARD RESET on page load
+  
   button.classList.remove("loading");
 
   button.addEventListener("click", () => {
